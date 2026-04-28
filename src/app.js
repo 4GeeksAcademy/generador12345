cc2e2e8e12db62eca4870e14b57f7b96289a1524
JuanDaxx03 <144707807+JuanDaxx03@users.noreply.github.com>
2026-04-28 01:27:36 +0000

    Generador hecho

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extensions = ['.com', '.net', '.us', '.io'];
+
+  let container = document.querySelector("#domains");
+  let domains = "";
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let e of extensions) {
+          let domain = p + a + n + e;
+          domains += `<p>${domain}</p>`;
+        }
+      }
+    }
+  }
+  container.innerHTML = domains;
 };
